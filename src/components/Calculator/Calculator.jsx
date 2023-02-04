7f0d7c10b8a6dfb42a4f2aa18de9ac00839b90c7
Júlia Acosta <juliagabriela98@gmail.com>
2023-02-04 15:44:34 -0300

    finish logic of calculator

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,59 +1,158 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CalculatorHeader from "../CalculatorHeader/CalculatorHeader";
 import Button from "../Button/Button";
 import styles from "./styles.module.css";
 
-const overFlow = 12;
+const overFlow = 15;
 
 export default function Calculator({ buttons }) {
-  const [operations, setOperations] = useState([]);
-  const [actualNumber, setActualNumber] = useState("");
-  const [result, setResult] = useState(0);
+    const [operations, setOperations] = useState([]);
+    const [result, setResult] = useState("");
 
-  const digitedNumber = (value) => {
-    setActualNumber(parseFloat(`${actualNumber}${value}`));
-    setResult(parseFloat(`${actualNumber}${value}`));
-  };
+    const mathOptions = (element, operation, nextElement) => {
+        const zeroDivision = element === 0 || nextElement === 0;
 
-  console.log(operations);
+        switch (true) {
+            case operation === "+":
+                return element + nextElement;
+            case operation === "-":
+                return element - nextElement;
+            case operation === "%":
+                return element % nextElement;
+            case operation === "x":
+                return element * nextElement;
+            case operation === "/":
+                if (zeroDivision) return 0;
+                return element / nextElement;
+        }
+    };
 
-  const addOperarations = (value) => {
-    const newOperations = [...operations];
-    if (typeof value === "string" && actualNumber !== null) {
-      newOperations.push(actualNumber);
-      newOperations.push(value);
-      setOperations(newOperations);
-      setActualNumber("");
-    } else {
-      digitedNumber(value);
-    }
-  };
+    const calculate = (operations) => {
+        let resultArray = [...operations];
+        let result;
 
-  const calculate = (value) => {
-    const newOperations = [];
-  };
-  // calculate(button.value)
-  return (
-    <div className={styles.calculator}>
-      <CalculatorHeader operations={operations} result={result} />
-      <main className={styles.rowsContainer}>
-        {buttons.map((rows) => (
-          <div className={styles.buttonsContainer}>
-            {rows.map((button) => {
-              return (
-                <Button
-                  styleName={button?.styles ? button.styles : undefined}
-                  onClick={() => {
-                    addOperarations(button.value);
-                  }}
-                >
-                  {button?.tag ? button.tag : button.value}
-                </Button>
-              );
-            })}
-          </div>
-        ))}
-      </main>
-    </div>
-  );
+        while (resultArray.length > 1) {
+            result = mathOptions(
+                resultArray[0],
+                resultArray[1],
+                resultArray[2]
+            );
+            resultArray.shift(); //removes from begin of the array
+            resultArray.shift();
+            resultArray.shift();
+            resultArray.unshift(result); // add from begin of the array
+        }
+
+        return resultArray[0];
+    };
+
+    const returnResult = (operation, operations) => {
+        const minimumToCalculate = operations.length >= 3;
+        if (minimumToCalculate) {
+            const result = calculate(operations);
+            setResult(result && operation === "%" ? result / 100 : result);
+            setOperations([]);
+        }
+    };
+
+    const restrictionBeforeCalculate = (operation, operations) => {
+        const cantUseOperation = result === "";
+
+        switch (true) {
+            case operation === "CE":
+                setResult("");
+                return true;
+            case operation === "C":
+                setResult("");
+                setOperations([]);
+                return true;
+            case operation === "=":
+                returnResult("=", operations);
+                return true;
+            case operation === "%":
+                returnResult("%", operations);
+                return true;
+            case cantUseOperation:
+                return true;
+            case operations.length >= overFlow:
+                alert("Overflow, to many operations");
+                return true;
+        }
+    };
+
+    const digitedNumber = (value) => {
+        if (value === "+/-") {
+            setResult(parseFloat(result) * -1);
+        } else if (value === ",") {
+            setResult(`${result}.`);
+        } else {
+            setResult(parseFloat(`${result}${value}`));
+        }
+    };
+
+    const addOperarations = (value) => {
+        const isAnOperation =
+            typeof value === "string" && value !== "," && value !== "+/-";
+
+        if (isAnOperation) {
+            const stop = restrictionBeforeCalculate(value, [
+                ...operations,
+                result,
+            ]);
+            if (stop) return;
+
+            setOperations((prevOperations) => [
+                ...prevOperations,
+                result, //number to calculate
+                value, //operation
+            ]);
+
+            setResult("");
+        } else {
+            digitedNumber(value);
+        }
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Escape") {
+            addOperarations("C");
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyPress, false);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress, false);
+        };
+    }, [handleKeyPress]);
+
+    return (
+        <div className={styles.calculator}>
+            <CalculatorHeader operations={operations} result={result} />
+            <main className={styles.rowsContainer}>
+                {buttons.map((rows, index) => (
+                    <div key={index} className={styles.buttonsContainer}>
+                        {rows.map((button) => {
+                            return (
+                                <Button
+                                    key={button.value}
+                                    styleName={
+                                        button?.styles
+                                            ? button.styles
+                                            : undefined
+                                    }
+                                    onClick={() => {
+                                        addOperarations(button.value);
+                                    }}
+                                >
+                                    {button?.tag ? button.tag : button.value}
+                                </Button>
+                            );
+                        })}
+                    </div>
+                ))}
+            </main>
+        </div>
+    );
 }
